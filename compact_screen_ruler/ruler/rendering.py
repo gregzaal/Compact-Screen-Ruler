f386a26fd349fc01807f38a5d2a1015f25beb910
Greg Zaal <gregzzmail@gmail.com>
2026-02-19 18:18:00 +0200

    Improve variable names

--- compact_screen_ruler/ruler/rendering.py
+++ compact_screen_ruler/ruler/rendering.py
@@ -17,42 +17,49 @@ class RulerRenderingMixin(
     """Coordinate paint flow using focused rendering mixins."""
 
     def paintEvent(self, _event):
-        col1 = 255 if not self.invert_colors else 0
-        col2 = 100 if not self.invert_colors else 155
-        col3 = 0 if not self.invert_colors else 255
+        highlight_gray = 255 if not self.invert_colors else 0
+        background_gray = 100 if not self.invert_colors else 155
+        stroke_gray = 0 if not self.invert_colors else 255
         self.resetResolutionTextState()
 
         painter = QtGui.QPainter()
         painter.begin(self)
 
-        pen = QtGui.QPen(QtGui.QColor(col3, col3, col3, 200), 1, QtCore.Qt.PenStyle.SolidLine)
-        painter.setPen(pen)
+        stroke_pen = QtGui.QPen(
+            QtGui.QColor(stroke_gray, stroke_gray, stroke_gray, 200), 1, QtCore.Qt.PenStyle.SolidLine
+        )
+        painter.setPen(stroke_pen)
 
         painter.setRenderHint(QtGui.QPainter.RenderHint.Antialiasing)
-        painter.setBrush(QtGui.QColor(col2, col2, col2, (0 if self.is_transparent else 180)))
+        painter.setBrush(
+            QtGui.QColor(background_gray, background_gray, background_gray, (0 if self.is_transparent else 180))
+        )
         painter.drawRoundedRect(QtCore.QRect(0, 0, self.width(), self.height()), 4, 4)
 
-        pen = QtGui.QPen(QtGui.QColor(col1, col1, col1, 0), 1, QtCore.Qt.PenStyle.SolidLine)
-        painter.setPen(pen)
+        transparent_pen = QtGui.QPen(
+            QtGui.QColor(highlight_gray, highlight_gray, highlight_gray, 0),
+            1,
+            QtCore.Qt.PenStyle.SolidLine,
+        )
+        painter.setPen(transparent_pen)
 
-        painter.setBrush(QtGui.QColor(col1, col1, col1, 25))
+        painter.setBrush(QtGui.QColor(highlight_gray, highlight_gray, highlight_gray, 25))
         if self.is_transparent:
             painter.drawRect(QtCore.QRect(0, 0, max(self.width(), 0), max(self.height(), 0)))
         else:
             painter.drawRect(QtCore.QRect(21, 21, max(self.width() - 21 * 2, 0), max(self.height() - 21 * 2, 0)))
 
-        self.drawHoverHints(painter, col1)
+        self.drawHoverHints(painter, highlight_gray)
         self.drawAlignedScreenEdges(painter)
 
-        pen = QtGui.QPen(QtGui.QColor(col3, col3, col3, 200), 1, QtCore.Qt.PenStyle.SolidLine)
-        painter.setPen(pen)
+        painter.setPen(stroke_pen)
 
         if not self.is_transparent:
             right_label_limit = self.getRightLabelLimit(painter)
-            x_tick_config, y_tick_config = self.drawSubticks(painter, col3)
-            self.drawMajorTicksAndLabels(painter, col3, right_label_limit, x_tick_config, y_tick_config)
+            x_tick_config, y_tick_config = self.drawSubticks(painter, stroke_gray)
+            self.drawMajorTicksAndLabels(painter, stroke_gray, right_label_limit, x_tick_config, y_tick_config)
 
             size_x, size_y = self.getMeasurementSize(painter)
-            self.drawResolutionReadout(painter, size_x, size_y, col3)
+            self.drawResolutionReadout(painter, size_x, size_y, stroke_gray)
 
         painter.end()
